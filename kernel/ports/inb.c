c4e093c2091035112624964e840dd560c9bcefeb
meles5 <meles5@web.de>
2015-06-21 13:12:36 +0200

    Fixed compile-bug

--- kernel/ports/inb.c
+++ kernel/ports/inb.c
@@ -1,7 +1,6 @@
 #include "stdint.h"
 #include "inb.h"
 
-<<<<<<< HEAD
 /*
 LFOS, a simple operating system.
 Copyright (C) 2015 LFUnion.
@@ -20,9 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-=======
 // Receives a value from a port
->>>>>>> 5e3761e9e541a3728459f05afd899d1bea34f1b0
 uint8_t inb(uint16_t port)
 {
     uint8_t value;
--- kernel/ports/outb.c
+++ kernel/ports/outb.c
@@ -1,7 +1,6 @@
 #include "stdint.h"
 #include "outb.h"
 
-<<<<<<< HEAD
 /*
 LFOS, a simple operating system.
 Copyright (C) 2015 LFUnion.
@@ -20,9 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-=======
 // Sends the port a value
->>>>>>> 5e3761e9e541a3728459f05afd899d1bea34f1b0
 void outb(uint16_t port, uint8_t value)
 {
     asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
