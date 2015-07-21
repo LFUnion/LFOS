0a031608a8c8aa984edd1e1623d64a7b74019e35
Fabian Stein <fenstein12@googlemai.com>
2015-07-21 16:08:24 +0200

    Added buildbot script

---
+++ buildbot.sh
@@ -0,0 +1,27 @@
+9#!/bin/bash
+
+# This script is not relevant to you if you are´nt the buildbot, but
+# feel free to explore it!
+
+# OUT_DIR and SOURCE_DIR are defined by a 
+# loader script on the buildserver. This script
+# also cleans the directorys after the build.
+
+# Set up required directorys
+rm -rf $OUT_DIR/*
+mkdir $OUT_DIR/logs
+mkdir $OUT_DIR/misc
+
+# Compile sources using Makefile
+cd $SOURCE_DIR
+make > $OUT_DIR/logs/build.log
+make mkiso > $OUT_DIR/logs/mkiso.log
+
+
+# Move files to the right directory
+mv $SOURCE_DIR/kernel/build/lfos.bin $OUT_DIR/lfos.bin
+mv $SOURCE_DIR/kernel/build/* $OUT_DIR/misc/
+mv $SOURCE_DIR/iso/lfos.iso $OUT_DIR/lfos.iso
+
+# Leave a timestamp
+echo "Build successfull. Time: " + $(date) > $OUT_DIR/timestamp
