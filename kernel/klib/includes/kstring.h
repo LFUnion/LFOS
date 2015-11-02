6295abd2ece8ae03eb994185370345ff31bd35db
stone3311 <fenstein12@googlemail.com>
2015-11-02 18:38:59 +0100

    Added strlen()

--- kernel/klib/includes/kstring.h
+++ kernel/klib/includes/kstring.h
@@ -4,5 +4,6 @@
 #include "stddef.h"
 
 void* memcpy(void* str1_ptr, void* str2_ptr, size_t n);
+int strlen(char* string);
 
 #endif /* KSTRING_H */
--- kernel/klib/kstring.c
+++ kernel/klib/kstring.c
@@ -12,3 +12,11 @@ void* memcpy(void* str1_ptr, void* str2_ptr, size_t n) {
     
     return str2_ptr;
 }
+
+int strlen(char* string) {
+    int len = 0;
+    for (int i = 0; string[i] != '\0'; i++) {
+	len++;
+    }
+    return len;
+}
