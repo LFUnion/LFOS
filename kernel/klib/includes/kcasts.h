ffd912a8b4ecc00149e7ba0c99f522fab9072fd1
stone3311 <fenstein12@googlemail.com>
2015-11-02 18:28:37 +0100

    Added ifndef to kcasts.h

--- kernel/klib/includes/kcasts.h
+++ kernel/klib/includes/kcasts.h
@@ -1,5 +1,10 @@
+#ifndef KCASTS_H
+#define KCASTS_H
+
 #include <stdint.h>
 
 int hexToInt(uint8_t input);
 const char * stringFromInt(int input);
 char charFromDigit(int digit);
+
+#endif /* KCASTS_H */
