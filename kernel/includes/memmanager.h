668dfc71892128784204143dee618d30091c01f8
TobiasBog100 <tobias.bogenfeld@t-online.de>
2015-12-01 21:11:59 +0100

    Update memmanager.h

--- kernel/includes/memmanager.h
+++ kernel/includes/memmanager.h
@@ -5,4 +5,20 @@
 
 void* malloc (size_t n);
 
+// new code
+
+void* ncalloc (size_t n, unsigned int size , double value);
+
+void func_scalloc(short value, short* count_ptr, long max);
+void func_icalloc(int value, int* count_ptr, long max);
+void func_lcalloc(long value, long* count_ptr, long max);
+void func_llcalloc(long long value, long long* count_ptr, long max);
+void func_fcalloc(float value, float* count_ptr, long max);
+void func_dcalloc(double value, double* count_ptr, long max);
+
+void* calloc(size_t n, unsigned int size);
+
+
+
+
 #endif /* MEMMANAGER_H */
