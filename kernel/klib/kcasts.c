1ef99c934140f0446ba251d0e7e87cd7927b6ff0
stone3311 <fenstein12@googlemail.com>
2016-04-22 14:26:28 +0200

    Updated charToInt function

--- kernel/klib/kcasts.c
+++ kernel/klib/kcasts.c
@@ -66,27 +66,8 @@ const char * stringFromInt(int input) {
  * @return The char
  */
 char charFromDigit(int digit) {
-    if (digit == 0) {
-	return '0';
-    } else if (digit == 1) {
-	return '1';
-    } else if (digit == 2) {
-	return '2';
-    } else if (digit == 3) {
-	return '3';
-    } else if (digit == 4) {
-	return '4';
-    } else if (digit == 5) {
-	return '5';
-    } else if (digit == 6) {
-	return '6';
-    } else if (digit == 7) {
-	return '7';
-    } else if (digit == 8) {
-	return '8';
-    } else if (digit == 9) {
-	return '9';
-    } else {
-	return '0';
-    }
+    if (digit >= 0 && digit <= 9)
+        return 48 + digit;
+    else
+        return '0';
 }
