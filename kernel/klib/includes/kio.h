f27704d11f50e26868eb381441279235e4337d97
TobiasBog100 <tobias.bogenfeld@t-online.de>
2016-05-24 11:50:01 +0000

    update header

--- kernel/klib/includes/kio.h
+++ kernel/klib/includes/kio.h
@@ -1,8 +1,11 @@
 #ifndef KIO_H
 #define KIO_H
 
+#include <stdint.h>
+
 void printf(const char text[], ...);
 char * scanf();
+void scanf_new_prot(char * content, uint16_t size);
 void printw(const char text[], ...);
 void print_raw(const char text[]);
 void printd(const int digit);
