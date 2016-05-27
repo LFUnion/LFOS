464fbe0652a5e0e52ef19333b7340ce4f1e77690
stone3311 <fenstein12@googlemail.com>
2016-05-27 16:20:38 +0200

    Added reg_state to IRQ handler

--- kernel/interrupts/irq.c
+++ kernel/interrupts/irq.c
@@ -2,9 +2,15 @@
 #include "klib.h"
 #include "pic.h"
 
-void irq_handler(uint32_t irq) {
-    /*print_raw((char*)itoa(irq));
+typedef struct {
+    uint32_t irq;
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t eip, cs, eflags, useresp, ss;
+} reg_state;
+
+void irq_handler(reg_state regs) {
+    /*print_raw((char*)itoa(regs.irq));
     print_raw(" ");*/
-    pic_eoi((int)irq);
+    pic_eoi((int)regs.irq);
     return;
 }
